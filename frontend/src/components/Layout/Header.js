e4a557eb3e88e3b34fa702ab67bd23e4dc776310
sudeepcv <basicsinjava@gmail.com>
2019-12-13 22:39:59 +0530

    title change

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -71,7 +71,7 @@ class Header extends Component {
       <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            Personal Project Management Tool
+            Sudeep Personal Project Management
           </Link>
           <button
             className="navbar-toggler"
